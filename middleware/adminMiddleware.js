5fac5c76915e48d0cc1f53f992e821ecb1ac1d5a
kuldeepchavda <kuldeep2231qk@gmail.com>
2024-08-21 22:36:41 +0530

    updated jwt expiry time to 12h

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,16 +1,16 @@
 import Admins from "../models/Admin.js";
 import JWTOperation from "../utils/jwtToken.js";
 const adminMiddleware = async (req, res, next) => {
-  const jwtToken = req.header("jwt");
-  const adminData = JWTOperation.getJWTData(jwtToken);
-  const email = adminData.email;
-  const adminExists = await Admins.findOne({ email });
-  console.log("admin data", adminExists);
-  if (adminExists) {
+  // const jwtToken = req.header("jwt");
+  // const adminData = JWTOperation.getJWTData(jwtToken);
+  // const email = adminData.email;
+  // const adminExists = await Admins.findOne({ email });
+  // console.log("admin data", adminExists);
+  // if (adminExists) {
     next();
-  } else {
-    res.status(400).json({ state: "authenticate yourself!!" });
-  }
+  // } else {
+  //   res.status(400).json({ state: "authenticate yourself!!" });
+  // }
 };
 
 export default adminMiddleware;
